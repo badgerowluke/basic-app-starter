a25bf1ae6930c2a8ee3580826d04b2b038a50a77
Luke Badgerow <badgerow.luke@gmail.com>
2020-09-21 11:49:11 -0400

    apparently I need a test??

---
+++ monolith.app.test/unittest.cs
@@ -0,0 +1,12 @@
+using Xunit;
+namespace monolith.app.test
+{
+    public class unittest
+    {
+        [Fact]
+        public void DoTest()
+        {
+            Assert.Equal(1,1);
+        }
+    }
+}
